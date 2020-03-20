aac187dc12db2d71428129210b006f4ddb57d524
Samuel James <bsbcraftplays@gmail.com>
2020-03-20 00:15:42 -0300

    Update ServerConnector.java

--- src/main/java/me/samxps/crafttunnel/netty/connector/ServerConnector.java
+++ src/main/java/me/samxps/crafttunnel/netty/connector/ServerConnector.java
@@ -5,15 +5,13 @@ import io.netty.channel.ChannelFuture;
 import io.netty.util.concurrent.Future;
 import me.samxps.crafttunnel.ProxyMode;
 import me.samxps.crafttunnel.server.ProxyServer;
-import me.samxps.crafttunnel.server.SlaveServer;
 
 /**
  * A {@link ServerConnector} is responsible for opening a channel to a
  * remote server. <br>
  * <br>
  * The {@link DirectServerConnector} will be used or on the
- * {@link ProxyMode#PROXY_ONLY} mode by the {@link ProxyServer} or on 
- * the {@link ProxyMode#MULTI_PROXY_TUNNEL} by the {@link SlaveServer}.<br>
+ * {@link ProxyMode#PROXY_ONLY} mode by the {@link ProxyServer}<br>
  * */
 public interface ServerConnector {
 
