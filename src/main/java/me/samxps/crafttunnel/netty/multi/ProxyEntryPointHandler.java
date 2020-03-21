32e9cb87f4d53d5f17649cfe4c9173feca9a331c
Samuel James <bsbcraftplays@gmail.com>
2020-03-21 16:36:20 -0300

    Virtual Channel implemetation on proxy entry and exit point hanlders

--- src/main/java/me/samxps/crafttunnel/netty/multi/ProxyEntryPointHandler.java
+++ src/main/java/me/samxps/crafttunnel/netty/multi/ProxyEntryPointHandler.java
@@ -1,14 +1,25 @@
 package me.samxps.crafttunnel.netty.multi;
 
+import java.net.InetSocketAddress;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.logging.Level;
 
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.util.concurrent.Future;
 import me.samxps.crafttunnel.CraftTunnel;
 import me.samxps.crafttunnel.ServerType;
+import me.samxps.crafttunnel.netty.channel.ClientChannelHandler;
+import me.samxps.crafttunnel.netty.channel.ServerChannelHandler;
 import me.samxps.crafttunnel.netty.connector.ServerConnector;
+import me.samxps.crafttunnel.netty.encode.MinecraftPacketEncoder;
+import me.samxps.crafttunnel.protocol.minecraft.MinecraftPacket;
+import me.samxps.crafttunnel.protocol.multi.WrapperPacket;
+import me.samxps.crafttunnel.protocol.multi.WrapperPacket.WrapperPacketType;
 
 /**
  * {@link ProxyEntryPointHandler} will be responsible for sending and receiving
@@ -17,6 +28,8 @@ import me.samxps.crafttunnel.netty.connector.ServerConnector;
 public class ProxyEntryPointHandler extends ChannelInboundHandlerAdapter {
 
 	private static HashSet<ProxyEntryPointHandler> instances = new HashSet<ProxyEntryPointHandler>();
+	private HashMap<InetSocketAddress, EmbeddedChannel> virtual = new HashMap<InetSocketAddress, EmbeddedChannel>();
+	
 	private Channel proxyChannel;
 	
 	private void info(String msg, Object data) {
@@ -41,12 +54,67 @@ public class ProxyEntryPointHandler extends ChannelInboundHandlerAdapter {
 		super.channelInactive(ctx);
 	}
 	
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		if (msg instanceof MinecraftPacket) {
+			MinecraftPacket p = (MinecraftPacket) msg;
+			
+			if (p.getPacketID() == WrapperPacket.getPacketID()) {
+				handlePacket(WrapperPacket.decode(p));
+			}
+		}
+	}
+	
+	private void handlePacket(WrapperPacket w) {
+		EmbeddedChannel vchannel = virtual.get(w.getClientAddress());
+		
+		if (vchannel == null)
+			 return;
+		
+		if (w.getType() == WrapperPacketType.CONNECTION_CLOSE) {
+			vchannel.close();
+		} else if (w.getType() == WrapperPacketType.DATA_BYTES) {
+			vchannel.writeOneInbound(w.getData());
+		} else if (w.getType() == WrapperPacketType.DATA_PACKET) {
+			vchannel.writeOneInbound(w.getData());
+		}
+	}
+	
+	private ChannelFuture newVirtualConnection(Channel clientChannel) {
+		InetSocketAddress clientAddress = ClientChannelHandler.getClientAddress(clientChannel);
+		
+		EmbeddedChannel ch = new EmbeddedChannel(
+				new ServerChannelHandler(clientChannel), 
+				new WrapperOutboundHandler(clientAddress, proxyChannel)
+		);
+		
+		virtual.put(clientAddress, ch);
+		
+		return proxyChannel.eventLoop().register(ch);
+	}
+	
 	/**
 	 * This will return a new virtual server connector
 	 * to send data trough the multiproxy tunnel
 	 * */
 	public static ServerConnector generateConnector() {
-		return null;
+		ProxyEntryPointHandler handler = instances.iterator().next();
+		return new ServerConnector() {
+			
+			private Channel ch;
+			
+			@Override
+			public ChannelFuture init(Channel clientChannel) throws Exception {
+				ChannelFuture f = handler.newVirtualConnection(clientChannel);
+				this.ch = f.channel();
+				return f;
+			}
+			
+			@Override
+			public Future<?> exit() throws Exception {
+				return ch.close();
+			}
+		};
 	}
 	
 }
--- src/main/java/me/samxps/crafttunnel/netty/multi/ProxyExitPointHandler.java
+++ src/main/java/me/samxps/crafttunnel/netty/multi/ProxyExitPointHandler.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import me.samxps.crafttunnel.ProxyConfiguration;
 import me.samxps.crafttunnel.netty.channel.ClientChannelHandler;
 import me.samxps.crafttunnel.netty.connector.DirectServerConnector;
+import me.samxps.crafttunnel.netty.encode.MinecraftPacketDecoder;
 import me.samxps.crafttunnel.protocol.minecraft.MinecraftPacket;
 import me.samxps.crafttunnel.protocol.multi.MagicPacket;
 import me.samxps.crafttunnel.protocol.multi.WrapperPacket;
@@ -25,12 +27,14 @@ import me.samxps.crafttunnel.protocol.multi.WrapperPacket.WrapperPacketType;
 public class ProxyExitPointHandler extends ChannelInboundHandlerAdapter {
 
 	private final ProxyConfiguration config;
-	private HashMap<InetSocketAddress, Channel> virtual = new HashMap<InetSocketAddress, Channel>();
+	private HashMap<InetSocketAddress, EmbeddedChannel> virtual = new HashMap<InetSocketAddress, EmbeddedChannel>();
 	public static AttributeKey<InetSocketAddress> PROXIED_CLIENT_ADDRESS = AttributeKey.valueOf("PROXIED_CLIENT_ADDRESS");
+	private Channel proxyChannel;
 	
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
 		// Sends magic packet to proxy entrypoint
+		proxyChannel = ctx.channel();
 		ctx.write(new MagicPacket().toMinecraftPacket());
 		ctx.flush();
 		super.channelActive(ctx);
@@ -52,22 +56,45 @@ public class ProxyExitPointHandler extends ChannelInboundHandlerAdapter {
 	
 	private void handleWrapper(WrapperPacket w) {
 		// TODO: map methods, direct access?
-		
+		InetSocketAddress clientAddress = w.getClientAddress();
 		if (w.getType() == WrapperPacketType.CONNECTION_START) {
-			EmbeddedChannel ch = new EmbeddedChannel(new ClientChannelHandler(DirectServerConnector.newDefault()));
-			ch.attr(PROXIED_CLIENT_ADDRESS).set(w.getClientAddress());
-			virtual.put(w.getClientAddress(), ch);
+			
+			EmbeddedChannel ch = new EmbeddedChannel(
+				new ClientChannelHandler(DirectServerConnector.newDefault()),
+				new WrapperOutboundHandler(clientAddress, proxyChannel)
+			);
+			
+			ch.attr(PROXIED_CLIENT_ADDRESS).set(clientAddress);
+			
+			ch.closeFuture().addListener(new ChannelFutureListener() {
+				
+				@Override
+				public void operationComplete(ChannelFuture future) throws Exception {
+					virtual.remove(clientAddress);
+				}
+			});
+			
+			proxyChannel.eventLoop().register(ch).addListener(new ChannelFutureListener() {
+				
+				@Override
+				public void operationComplete(ChannelFuture future) throws Exception {
+					System.out.println("yay! " + future.isSuccess());
+				}
+			});
+			
+			virtual.put(clientAddress, ch);
 		} else {
-			Channel vchannel = virtual.get(w.getClientAddress());
+			EmbeddedChannel vchannel = virtual.get(clientAddress);
 			
 			if (vchannel == null) return;
 			
 			if (w.getType() == WrapperPacketType.CONNECTION_CLOSE) {
-				
+				vchannel.close();
 			} else if (w.getType() == WrapperPacketType.DATA_BYTES) {
-				
+				vchannel.writeOneInbound(w.getData());
 			} else if (w.getType() == WrapperPacketType.DATA_PACKET) {
-				
+				// Should we decode the packet ?
+				vchannel.writeOneInbound(w.getData());
 			}
 		}
 	}
